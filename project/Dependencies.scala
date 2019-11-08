e665e9d211dce3b95fc89b0140b967f3edc55dec
Jeff Boutotte <jeff_boutotte@ultimatesoftware.com>
2019-11-08 13:16:24 -0500

    Embedded engine test helper actually reads from Kafka for integration testing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,8 +21,8 @@ object Dependencies extends AutoPlugin {
 
     object Ultimate {
       object Surge {
-        val common = "com.ultimatesoftware" %% "surge-common" % "0.1.4"
-        val scalaCore = "com.ultimatesoftware" %% "ulti-scala-core" % "0.1.0"
+        val common = "com.ultimatesoftware" %% "surge-common" % "0.1.5"
+        val scalaCore = "com.ultimatesoftware" %% "ulti-scala-core" % "0.1.2"
       }
     }
 
---
+++ test-engine-javadsl/src/main/scala/com/ultimatesoftware/kafka/streams/javadsl/test/EmbeddedEngineTestHelper.scala
@@ -0,0 +1,68 @@
+// Copyright © 2017-2019 Ultimate Software Group. <https://www.ultimatesoftware.com>
+
+package com.ultimatesoftware.kafka.streams.javadsl.test
+
+import java.util.UUID
+import java.util.concurrent.{ Callable, TimeUnit }
+
+import com.ultimatesoftware.kafka.streams.javadsl.UltiKafkaStreamsCommandBusinessLogic
+import com.ultimatesoftware.scala.core.domain.StatePlusMetadata
+import com.ultimatesoftware.scala.core.kafka.{ KafkaBytesConsumer, UltiKafkaConsumerConfig }
+import com.ultimatesoftware.scala.core.messaging.{ EventMessage, EventMessageSerializer, EventMessageSerializerRegistry }
+import com.ultimatesoftware.scala.core.utils.JsonUtils
+import org.awaitility.Awaitility._
+import org.awaitility.core.ThrowingRunnable
+import org.slf4j.{ Logger, LoggerFactory }
+import play.api.libs.json.Format
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+
+class EmbeddedEngineTestHelper[AggId, Agg, Cmd, Event, CmdMeta](
+    kafkaBrokers: java.util.List[String],
+    eventMessageSerializers: java.util.List[EventMessageSerializer[_ <: Event]],
+    val ultiBusinessLogic: UltiKafkaStreamsCommandBusinessLogic[AggId, Agg, Cmd, Event, CmdMeta]) {
+  private val timeoutSeconds = 30
+  private val log: Logger = LoggerFactory.getLogger(getClass)
+
+  val eventBus: EventBus[EventMessage[Event]] = new EventBus[EventMessage[Event]]
+  val stateStore: StateStore[AggId, StatePlusMetadata[Agg]] = new StateStore[AggId, StatePlusMetadata[Agg]]
+
+  def clearReceivedEvents(): Unit = {
+    eventBus.clear()
+  }
+
+  private implicit def byNameFunctionToCallableOfType[T](function: ⇒ T): Callable[T] = () ⇒ function
+  private implicit def byNameFunctionToCallableOfBoolean(function: ⇒ scala.Boolean): Callable[java.lang.Boolean] = () ⇒ function
+  private implicit def byNameFunctionToRunnable[T](function: ⇒ T): ThrowingRunnable = () ⇒ function
+
+  def waitForEvent[T](`type`: Class[T]): T = {
+    var event: Option[T] = None
+    await().atMost(timeoutSeconds, TimeUnit.SECONDS) until {
+      eventBus.consumeOne().map(_.body) match {
+        case Some(evt) if evt.getClass == `type` ⇒
+          event = Some(evt.asInstanceOf[T])
+        case _ ⇒
+        // Do nothing
+      }
+      event.nonEmpty
+    }
+
+    event.getOrElse(throw new SurgeAssertionError(s"Event of type ${`type`} not found in the eventbus"))
+  }
+
+  private val eventMessageSerializerRegistry = EventMessageSerializerRegistry[Event](eventMessageSerializers.asScala)
+  private implicit val eventFormatter: Format[EventMessage[Event]] = eventMessageSerializerRegistry.formatFromSchemaRegistry
+  private val consumerGroupName = s"SurgeEmbeddedEngineTestHelper-${UUID.randomUUID()}"
+  private val consumerConfig = UltiKafkaConsumerConfig(consumerGroupName, offsetReset = "latest", pollDuration = 1.second)
+  private val sub = KafkaBytesConsumer(kafkaBrokers.asScala, consumerConfig, Map.empty)
+  sub.subscribe(ultiBusinessLogic.eventsTopic, { record ⇒
+    JsonUtils.parseMaybeCompressedBytes[EventMessage[Event]](record.value()) match {
+      case Some(eventMessage) ⇒
+        eventBus.send(eventMessage)
+      case _ ⇒
+        log.error(s"Unable to interpret value from Kafka ${record.value()}")
+    }
+  })
+
+}
