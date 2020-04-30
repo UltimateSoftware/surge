2072b2656a52a8215796cf1938f2ce90e283e326
Alberto Perez <alberto_perez@ultimatesoftware.com>
2020-04-30 16:35:17 -0400

    Changing Java HealthCheck to a java class instead of using case class

--- modules/command-engine/javadsl/src/main/scala/com/ultimatesoftware/kafka/streams/javadsl/HealthCheckTrait.scala
+++ modules/command-engine/javadsl/src/main/scala/com/ultimatesoftware/kafka/streams/javadsl/HealthCheckTrait.scala
@@ -4,28 +4,34 @@ package com.ultimatesoftware.kafka.streams.javadsl
 
 import java.util.Optional
 import java.util.concurrent.CompletionStage
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
 import scala.collection.JavaConverters._
 import com.ultimatesoftware.kafka.streams.{ HealthCheck ⇒ ScalaHealthCheck }
+import com.ultimatesoftware.scala.core.utils.JsonFormats
+
 import scala.compat.java8.OptionConverters._
 
-case class HealthCheck(
-    name: String,
-    id: String,
-    status: String,
-    components: Optional[java.util.List[HealthCheck]],
-    details: Optional[java.util.Map[String, String]])
+class HealthCheck(
+    val name: String,
+    val id: String,
+    val status: String,
+    val components: Optional[java.util.List[HealthCheck]],
+    val details: Optional[java.util.Map[String, String]])
 
 object HealthCheck {
+  implicit val writer = JsonFormats.jacksonWriter[HealthCheck]
   implicit class HealthCheckToJavaConverter(scalaHealthCheck: ScalaHealthCheck) {
     def asJava: HealthCheck = {
-      HealthCheck(
-        name = scalaHealthCheck.name,
-        id = scalaHealthCheck.id,
-        status = scalaHealthCheck.status,
-        components = scalaHealthCheck.components.map(scalaHealthCheckList ⇒
+      new HealthCheck(
+        scalaHealthCheck.name,
+        scalaHealthCheck.id,
+        scalaHealthCheck.status,
+        scalaHealthCheck.components.map(scalaHealthCheckList ⇒
           scalaHealthCheckList.map(scalaHealthCheck ⇒
             scalaHealthCheck.asJava).asJava).asJava,
-        details = scalaHealthCheck.details.map(_.asJava).asJava)
+        scalaHealthCheck.details.map(_.asJava).asJava)
     }
   }
 }
