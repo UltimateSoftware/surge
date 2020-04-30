54e143a9587fac91eff975c0721ea8c9255664c9
Jeff Boutotte <jeff_boutotte@ultimatesoftware.com>
2020-04-30 15:35:10 -0400

    Clean up GenericAggregateActor

--- modules/command-engine/core/src/main/scala/com/ultimatesoftware/kafka/streams/core/GenericAggregateActor.scala
+++ modules/command-engine/core/src/main/scala/com/ultimatesoftware/kafka/streams/core/GenericAggregateActor.scala
@@ -9,8 +9,7 @@ import akka.actor.{ Actor, Props, ReceiveTimeout, Stash }
 import akka.pattern.pipe
 import com.ultimatesoftware.akka.cluster.Passivate
 import com.ultimatesoftware.config.TimeoutConfig
-import com.ultimatesoftware.kafka.streams.HealthyActor.GetHealth
-import com.ultimatesoftware.kafka.streams.{ AggregateStateStoreKafkaStreams, HealthCheck, HealthCheckStatus }
+import com.ultimatesoftware.kafka.streams.AggregateStateStoreKafkaStreams
 import com.ultimatesoftware.scala.core.monitoring.metrics.{ MetricsProvider, Timer }
 import com.ultimatesoftware.scala.core.validations._
 import org.slf4j.LoggerFactory
@@ -113,7 +112,7 @@ private[core] class GenericAggregateActor[AggId, Agg, Command, Event, CmdMeta, E
 
   override def receive: Receive = uninitialized
 
-  private def freeToProcess(state: InternalActorState): Receive = healthCheckReceiver orElse {
+  private def freeToProcess(state: InternalActorState): Receive = {
     case msg: CommandEnvelope[AggId, Command, CmdMeta] ⇒ handle(state, msg)
     case msg: ApplyEventEnvelope[AggId, Event, EvtMeta] ⇒ handle(state, msg)
     case _: GetState[AggId] ⇒ sender() ! state.stateOpt
@@ -129,27 +128,13 @@ private[core] class GenericAggregateActor[AggId, Agg, Command, Event, CmdMeta, E
     case _                                ⇒ stash()
   }
 
-  private def uninitialized: Receive = healthCheckReceiver orElse {
+  private def uninitialized: Receive = {
     case msg: InitializeWithState ⇒ handle(msg)
     case ReceiveTimeout ⇒
       // Ignore and drop ReceiveTimeout messages from this state
       log.warn(s"Aggregate actor for $aggregateId received a ReceiveTimeout message in uninitialized state. " +
         "This should not happen and is likely a logic error. Dropping the ReceiveTimeout message.")
     case _ ⇒ stash()
-  }
-
-  private def healthCheckReceiver(): Receive = {
-    case GetHealth ⇒ getHealth()
-  }
-
-  private def getHealth() = {
-    kafkaProducerActor.healthCheck().map { producerHealthCheck ⇒
-      HealthCheck(
-        id = aggregateId.toString,
-        name = "aggregate-actor",
-        status = HealthCheckStatus.UP,
-        components = Some(Seq(producerHealthCheck)))
-    }.pipeTo(sender())
   }
 
   private def handle(state: InternalActorState, commandEnvelope: CommandEnvelope[AggId, Command, CmdMeta]): Unit = {
