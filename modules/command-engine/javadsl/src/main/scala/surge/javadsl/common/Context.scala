edaca7b27b9ec0be67745fd0ea26aa3ffa793a54
Chris McDermott <christopher.mcdermott@ukg.com>
2021-06-10 16:01:32 -0400

    Use DSL specific types for contexts. (#16)

---
+++ modules/command-engine/javadsl/src/main/scala/surge/javadsl/common/Context.scala
@@ -0,0 +1,14 @@
+// Copyright © 2017-2021 UKG Inc. <https://www.ukg.com>
+
+package surge.javadsl.common
+
+import surge.internal
+import surge.internal.persistence
+
+trait Context {}
+
+object Context {
+  def apply(core: persistence.Context): Context = new ContextImpl(core)
+}
+
+class ContextImpl(private val core: persistence.Context) extends Context
