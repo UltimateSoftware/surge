197fb1daecb1ee84fe6d4c2a8547105178f3b0bd
Sebastian Harko <sebastian.harko@protonmail.com>
2021-07-14 12:10:25 +0200

    add some documentation for actor with tracing

--- modules/common/src/main/scala/surge/tracing/ActorWithTracing.scala
+++ modules/common/src/main/scala/surge/tracing/ActorWithTracing.scala
@@ -6,6 +6,36 @@ import akka.actor.{ Actor, ActorRef }
 import io.opentracing.{ Span, Tracer }
 import surge.internal.utils.SpanExtensions
 
+// format: off
+/*
+ How to use this trait:
+
+ class MyActor extends ActorWithTracing {
+
+   // optional
+   override def extractMessageName = {
+     case m: Envelope[_] => s"Envelope[${m.payload.getClass.getSimpleName}]"
+   }
+
+   override def receive: Receive = traceableMessages {
+     span => {
+       case e: Envelope[_] => // Envelope[DoWork]
+        // the new span is named "MyActor:Envelope[DoWork]"
+        doSomeWork()
+        span.log("some work done!")
+        doEvenMoreWork()
+        span.log("even more work done!")
+        otherActor ! TracedMessage(WorkDone(), parentSpan = span)
+        // no need to call span.finish() - we call it for you automatically
+     }
+  }.orElse {
+    // messages we do not want to trace go here
+    case OtherMessage => doStuff()
+  }
+ }
+*/
+// format: on
+
 trait ActorWithTracing extends Actor with ActorOps with SpanExtensions {
 
   implicit val tracer: Tracer
@@ -13,18 +43,14 @@ trait ActorWithTracing extends Actor with ActorOps with SpanExtensions {
   private def actorClassFullName: String = this.getClass.getName
 
   private def actorClassSimpleName: String = this.getClass.getSimpleName
+  def extractMessageName: PartialFunction[Any, String] = PartialFunction.empty[Any, String]
 
-  private def getMessageName(tracedMsg: TracedMessage[_]): String = {
-    // we could potentially use reflection:
-    // https://stackoverflow.com/a/60722477
-    /*
-    import scala.reflect.runtime.universe._
-    def getClassName[T](x: T)(implicit tag: TypeTag[T]): String = {
-      tag.tpe match { case TypeRef(_, t, args) => s"""${t.name} [${args.mkString(",")}]""" }
+  private def getMessageName(msg: Any): String = {
+    if (extractMessageName.isDefinedAt(msg)) {
+      extractMessageName(msg)
+    } else {
+      msg.getClass.getSimpleName
     }
-    getClassName(tracedMsg)
-     */
-    tracedMsg.messageName
   }
 
   def traceableMessages(userReceive: ActorReceiveSpan => Actor.Receive): Actor.Receive = new Actor.Receive {
@@ -36,18 +62,21 @@ trait ActorWithTracing extends Actor with ActorOps with SpanExtensions {
     override def apply(msg: Any): Unit = {
       msg match {
         case tracedMsg: TracedMessage[_] =>
-          val span: Span = Tracing.childFrom(tracedMsg, operationName = s"${actorClassSimpleName}:${getMessageName(tracedMsg)}")
+          val messageName: String = getMessageName(tracedMsg.message)
+          val operationName: String = s"$actorClassSimpleName:$messageName"
+          val span: Span = Tracing.childFrom(tracedMsg, operationName)
           val actorReceiveSpan = ActorReceiveSpan(span, tracedMsg.messageName)
           val fields = Map(
-            "actor class (FQCN)" -> actorClassFullName,
+            "actor" -> actorClassSimpleName,
+            "actor (fqcn)" -> actorClassFullName,
+            "message" -> messageName,
+            "message (fqcn)" -> tracedMsg.message.getClass.getName,
             "receiver path" -> self.prettyPrintPath,
-            "sender path" -> sender().prettyPrintPath,
-            "message (given name)" -> getMessageName(tracedMsg),
-            "message (FQCN)" -> tracedMsg.message.getClass.getName)
+            "sender path" -> sender().prettyPrintPath)
           if (userReceive(actorReceiveSpan).isDefinedAt(tracedMsg.message)) {
-            span.log(s"receive", fields)
+            span.log("receive", fields)
             userReceive(actorReceiveSpan)(tracedMsg.message)
-            span.log(s"done")
+            span.log("done")
             span.finish()
           } else {
             span.log("lost", fields)
