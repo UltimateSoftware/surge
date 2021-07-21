58fcad0ffec41319e44785ef84dd5bcd0126b87b
Sebastian Harko <sebastian.harko@protonmail.com>
2021-07-21 13:21:45 +0200

    scalafmt

--- modules/common/src/test/scala/surge/kafka/KafkaPartitionShardRouterActorSpec.scala
+++ modules/common/src/test/scala/surge/kafka/KafkaPartitionShardRouterActorSpec.scala
@@ -2,8 +2,8 @@
 
 package surge.kafka
 
-import akka.actor.{Actor, ActorContext, ActorSystem, DeadLetter, Props}
-import akka.testkit.{TestKit, TestProbe}
+import akka.actor.{ Actor, ActorContext, ActorSystem, DeadLetter, Props }
+import akka.testkit.{ TestKit, TestProbe }
 import org.apache.kafka.common.TopicPartition
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.when
@@ -12,11 +12,11 @@ import org.scalatest.Resources.should
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatestplus.mockito.MockitoSugar
-import surge.akka.cluster.{EntityPropsProvider, PerShardLogicProvider}
+import surge.akka.cluster.{ EntityPropsProvider, PerShardLogicProvider }
 import surge.internal.akka.cluster.ActorSystemHostAwareness
-import surge.internal.akka.kafka.{KafkaConsumerPartitionAssignmentTracker, KafkaConsumerStateTrackingActor}
-import surge.kafka.streams.{HealthCheck, HealthCheckStatus}
-import surge.internal.tracing.{NoopTracerFactory, TracedMessage}
+import surge.internal.akka.kafka.{ KafkaConsumerPartitionAssignmentTracker, KafkaConsumerStateTrackingActor }
+import surge.kafka.streams.{ HealthCheck, HealthCheckStatus }
+import surge.internal.tracing.{ NoopTracerFactory, TracedMessage }
 
 import scala.concurrent.Future
 
