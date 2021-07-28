30c58cc15cb284c7f32ec82b9c7f425d3039683d
Sebastian Harko <sebastian.harko@protonmail.com>
2021-07-28 12:06:19 +0200

    checkpoint

--- modules/common/src/multi-jvm/scala/surge/internal/streams/StreamManagerMultiJvmSpec.scala
+++ modules/common/src/multi-jvm/scala/surge/internal/streams/StreamManagerMultiJvmSpec.scala
@@ -5,10 +5,10 @@ package surge.internal.streams
 import akka.NotUsed
 import akka.kafka.Subscriptions
 import akka.remote.testconductor.RoleName
-import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec, MultiNodeSpecCallbacks}
+import akka.remote.testkit.{ MultiNodeConfig, MultiNodeSpec, MultiNodeSpecCallbacks }
 import akka.stream.scaladsl.Flow
 import akka.testkit.TestProbe
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{ Config, ConfigFactory }
 import io.opentelemetry.api.trace.Tracer
 import net.manub.embeddedkafka._
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -22,7 +22,7 @@ import surge.internal.tracing.NoopTracerFactory
 import surge.kafka.KafkaTopic
 import surge.kafka.streams.DefaultSerdes
 import surge.streams.replay._
-import surge.streams.{DataHandler, EventPlusStreamMeta}
+import surge.streams.{ DataHandler, EventPlusStreamMeta }
 
 import scala.concurrent.duration._
 import scala.language.implicitConversions
