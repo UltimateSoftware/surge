1396c8bdc7195818a656843ef973b55b9e0cea8d
Sebastian Harko <sebastian.harko@protonmail.com>
2021-07-30 10:32:26 +0200

    formmat

--- modules/metrics/src/test/scala/surge/metrics/TimerSpec.scala
+++ modules/metrics/src/test/scala/surge/metrics/TimerSpec.scala
@@ -45,7 +45,6 @@ class TimerSpec extends TestKit(ActorSystem("TimerSpec")) with MetricsSpecLike w
       val testTimerName = "future-timer-test"
       val timer = metrics.timer(MetricInfo(testTimerName, "Test timer description"))
 
-
       timer.timeFuture { akka.pattern.after(50.millis)(Future.successful(())) }
 
       val marginErr = 25.0
