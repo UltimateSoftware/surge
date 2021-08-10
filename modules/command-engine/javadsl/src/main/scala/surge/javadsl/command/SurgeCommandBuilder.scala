af7632ddafcf49127df160fc64edd8fb676e3dec
lteqgt <5114916+sebastianharko@users.noreply.github.com>
2021-08-10 15:47:49 +0200

    Java DSL improvements (plus tune some of the timer spec tests..) (#60)
    * builder pattern
    
    * java dsl improvements
    
    * other things
    
    * disable test
    
    * fix test
    
    * fix test

---
+++ modules/command-engine/javadsl/src/main/scala/surge/javadsl/command/SurgeCommandBuilder.scala
@@ -0,0 +1,20 @@
+// Copyright © 2017-2021 UKG Inc. <https://www.ukg.com>
+
+package surge.javadsl.command
+
+import surge.core.commondsl.SurgeCommandBusinessLogicTrait
+
+class Buildable[AggId, Agg, Command, Evt](businessLogic: SurgeCommandBusinessLogicTrait[AggId, Agg, Command, Evt]) {
+  def build(): SurgeCommand[AggId, Agg, Command, Nothing, Evt] = {
+    SurgeCommand.create(businessLogic)
+  }
+}
+
+class SurgeCommandBuilder {
+
+  def withBusinessLogic[AggId, Agg, Command, Evt](
+      businessLogic: SurgeCommandBusinessLogicTrait[AggId, Agg, Command, Evt]): Buildable[AggId, Agg, Command, Evt] = {
+    new Buildable[AggId, Agg, Command, Evt](businessLogic)
+  }
+
+}
