343c2df34ef34d30a945813d2cc6b6c9660be584
zachary-rote <zachary-rote@github.com>
2021-08-26 11:01:43 -0400

    style format

--- modules/common/src/main/scala/surge/streams/DataPipeline.scala
+++ modules/common/src/main/scala/surge/streams/DataPipeline.scala
@@ -26,7 +26,6 @@ object DataPipeline {
   // This is a case class on purpose, Kotlin doesn't do pattern matching against scala case objects :(
   case class ReplaySuccessfullyStarted()
       extends ReplayResult
-  case class ReplaySuccessfullyCompleted() extends ReplayResult
   case class ReplayFailed(reason: Throwable) extends ReplayResult
 }
 
