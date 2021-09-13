b6819d29a396cdd62cc4412f437506bc73f918e7
lteqgt <5114916+sebastianharko@users.noreply.github.com>
2021-09-13 15:48:57 +0200

    gRPC multilanguage server (#80)

---
+++ modules/multilanguage-scala-sdk/src/main/scala/com/ukg/surge/multilanguage/scalasdk/Model.scala
@@ -0,0 +1,37 @@
+package com.ukg.surge.multilanguage.scalasdk
+
+import com.google.protobuf.ByteString
+
+import scala.util.Try
+
+final case class CQRSModel[S, E, C](eventHandler: (Option[S], E) => Option[S], commandHandler: (Option[S], C) => Either[String, Seq[E]]) {
+
+  private[scalasdk] def applyEvents(s: Option[S], e: Seq[E]): Option[S] = {
+    e.foldLeft(s)((s, e) => eventHandler(s, e))
+  }
+
+  private[scalasdk] def executeCommand(s: Option[S], c: C): Either[String, (Seq[E], Option[S])] = {
+    commandHandler(s, c).map(events => (events, applyEvents(s, events)))
+  }
+}
+
+final case class SerDeser[S, E, C](
+    deserializeState: Array[Byte] => Try[S],
+    deserializeEvent: Array[Byte] => Try[E],
+    deserializeCommand: Array[Byte] => Try[C],
+    serializeState: S => Try[Array[Byte]],
+    serializeEvent: E => Try[Array[Byte]]) {
+
+  def deserializeState(byteString: ByteString): Try[S] = {
+    deserializeState(byteString.toByteArray)
+  }
+
+  def deserializeCommand(byteString: ByteString): Try[C] = {
+    deserializeCommand(byteString.toByteArray)
+  }
+
+  def deserializeEvent(byteString: ByteString): Try[E] = {
+    deserializeEvent(byteString.toByteArray)
+  }
+
+}
---
+++ modules/multilanguage/src/main/scala/com/ukg/surge/multilanguage/GenericAsyncAggregateCommandModel.scala
@@ -0,0 +1,87 @@
+// Copyright © 2017-2021 UKG Inc. <https://www.ukg.com>
+
+package com.ukg.surge.multilanguage
+
+import akka.actor.ActorSystem
+import akka.event.{ Logging, LoggingAdapter }
+import com.ukg.surge.multilanguage
+import com.ukg.surge.multilanguage.protobuf._
+import surge.scaladsl.command.AsyncAggregateCommandModel
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+class GenericAsyncAggregateCommandModel(bridgeToBusinessApp: BusinessLogicService)(implicit system: ActorSystem)
+    extends AsyncAggregateCommandModel[SurgeState, SurgeCmd, SurgeEvent] {
+
+  import Implicits._
+  val logger: LoggingAdapter = Logging(system, classOf[GenericAsyncAggregateCommandModel])
+  import system.dispatcher
+
+  override def executionContext: ExecutionContext = system.dispatcher
+
+  def validIds(aggregate: Option[SurgeState], surgeCmd: SurgeCmd): Boolean = {
+    aggregate.map(_.aggregateId) match {
+      case Some(value) => surgeCmd.aggregateId == value
+      case None        => true
+    }
+  }
+
+  def validIds(aggregate: Option[SurgeState], surgeEvents: Seq[SurgeEvent]): Boolean = {
+    val aggsIds = surgeEvents.map(_.aggregateId).distinct
+    aggregate.map(_.aggregateId) match {
+      case Some(value) =>
+        aggsIds.size == 1 && aggsIds.distinct.head == value
+      case None =>
+        aggsIds.size == 1
+    }
+  }
+
+  override def processCommand(aggregate: Option[SurgeState], surgeCommand: SurgeCmd): Future[Seq[SurgeEvent]] = {
+    if (!validIds(aggregate, surgeCommand)) {
+      Future.failed(new Exception("Wrong aggregate ids!"))
+    } else {
+      logger.info(s"""Calling command handler of business app via gRPC.
+           |Aggregate id: ${surgeCommand.aggregateId}).
+           |State defined: ${aggregate.isDefined}.
+           |Command payload size: ${surgeCommand.payload.length} (bytes).""".stripMargin)
+
+      val maybePbState: Option[protobuf.State] = aggregate.map(surgeState => surgeState: protobuf.State)
+      val pbCommand: protobuf.Command = surgeCommand: multilanguage.protobuf.Command
+      val processCommandRequest = ProcessCommandRequest(aggregateId = surgeCommand.aggregateId, maybePbState, Some(pbCommand))
+      val reply: Future[ProcessCommandReply] = bridgeToBusinessApp.processCommand(processCommandRequest)
+
+      reply.flatMap { processCommandReply =>
+        {
+          if (processCommandReply.isSuccess) {
+            Future.successful {
+              processCommandReply.events.map(e => e: SurgeEvent)
+            }
+          } else {
+            Future.failed(new Exception(processCommandReply.rejectionMessage))
+          }
+        }
+      }
+    }
+  }
+
+  override def handleEvents(aggregate: Option[SurgeState], surgeEvents: Seq[SurgeEvent]): Future[Option[SurgeState]] = {
+    if (surgeEvents.isEmpty) {
+      Future.failed(new Exception("No events provided!"))
+    } else {
+      if (!validIds(aggregate, surgeEvents)) {
+        Future.failed(new Exception("Wrong aggregate ids!"))
+      } else {
+        val aggregateId = aggregate.map(_.aggregateId).orElse(surgeEvents.headOption.map(_.aggregateId)).get
+        logger.info(s"""Calling event handler of business app via gRPC.
+             |Aggregate id: ${aggregateId}
+             |State defined: ${aggregate.isDefined}.
+             |Num events: ${surgeEvents.size}.
+             |Event payload sizes (bytes): ${surgeEvents.map(_.payload.length).mkString(",")}.""".stripMargin)
+        val maybePbState: Option[protobuf.State] = aggregate.map(surgeState => surgeState: protobuf.State)
+        val handleEventRequest = HandleEventsRequest(aggregateId, maybePbState, surgeEvents.map(surgeEvent => surgeEvent: protobuf.Event))
+        val reply: Future[HandleEventsResponse] = bridgeToBusinessApp.handleEvents(handleEventRequest)
+        reply.map { r => r.state.map(state => state: SurgeState) }
+      }
+    }
+  }
+}
