21aaf13bf97d35b8b1a450229936e547c2a91a19
Jeff Boutotte <jeff.boutotte@ukg.com>
2021-09-22 14:39:50 -0400

    Add support for AsyncEventModel to the javadsl

--- modules/command-engine/javadsl/src/main/scala/surge/javadsl/event/AggregateEventModel.scala
+++ modules/command-engine/javadsl/src/main/scala/surge/javadsl/event/AggregateEventModel.scala
@@ -3,17 +3,28 @@
 package surge.javadsl.event
 
 import surge.core.event.AggregateEventModelCoreTrait
-import surge.internal
-import surge.internal.domain.EventHandler
+import surge.internal.domain.{ AsyncEventHandler, EventHandler }
 import surge.internal.persistence
 import surge.javadsl.common.Context
 
 import java.util.Optional
+import java.util.concurrent.CompletableFuture
+import scala.compat.java8.FutureConverters
 import scala.compat.java8.OptionConverters._
+import scala.concurrent.{ ExecutionContext, Future }
 
 trait AggregateEventModel[Agg, Evt] extends AggregateEventModelCoreTrait[Agg, Evt] {
   def handleEvent(ctx: Context, state: Optional[Agg], event: Evt): Optional[Agg]
 
   override def toCore: EventHandler[Agg, Evt] = (ctx: persistence.Context, state: Option[Agg], event: Evt) =>
     handleEvent(Context(ctx), state.asJava, event).asScala
+}
+
+trait AsyncAggregateEventModel[Agg, Evt] extends AggregateEventModelCoreTrait[Agg, Evt] {
+  def handleEvent(ctx: Context, state: Optional[Agg], event: Evt): CompletableFuture[Optional[Agg]]
+
+  override final def toCore: EventHandler[Agg, Evt] = new AsyncEventHandler[Agg, Evt] {
+    override def handleEventAsync(ctx: persistence.Context, state: Option[Agg], event: Evt): Future[Option[Agg]] =
+      FutureConverters.toScala(AsyncAggregateEventModel.this.handleEvent(Context(ctx), state.asJava, event)).map(_.asScala)(ExecutionContext.global)
+  }
 }
