f422470d5d5947cfa5c8dcfd0ae31f1fa6938197
zachary-rote <zachary-rote@github.com>
2021-09-29 09:52:49 -0400

    remove @ignore and comment

--- modules/command-engine/core/src/test/scala/surge/internal/domain/SurgeMessagePipelineSpec.scala
+++ modules/command-engine/core/src/test/scala/surge/internal/domain/SurgeMessagePipelineSpec.scala
@@ -29,8 +29,6 @@ import surge.metrics.Metrics
 import java.util.regex.Pattern
 import scala.concurrent.duration._
 
-// fix: flaky test
-//@Ignore
 class SurgeMessagePipelineSpec
     extends TestKit(ActorSystem("SurgeMessagePipelineSpec", ConfigFactory.load("artery-test-config")))
     with AnyWordSpecLike
