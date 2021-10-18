7f3df55f47ee835f91c7b106c345a285df7131fc
Sebastian Harko <sebastian.harko@protonmail.com>
2021-10-18 12:53:57 +0200

    continue work

--- modules/multilanguage-csharp-sdk/Sample/Program.cs
+++ modules/multilanguage-csharp-sdk/Sample/Program.cs
@@ -15,96 +15,94 @@ namespace Surge.Sample
         static void Main(string[] args)
         {
 
-            var cqrsModel = new CqrsModel<Account, BankEvent, BankCommand>();
-
-            cqrsModel.EventHandler = input =>
+            var cqrsModel = new CqrsModel<Account, BankEvent, BankCommand>
             {
-                Option<Account> state;
-                BankEvent bankEvent;
-                // ReSharper disable once SuggestVarOrType_Elsewhere
-                (state, bankEvent) = input;
-                var balance = state.IsSome switch
+                EventHandler = input =>
                 {
-                    true => state.ToList().Head().amount,
-                    _ => 0
-                };
+                    Option<Account> state;
+                    BankEvent bankEvent;
+                    // ReSharper disable once SuggestVarOrType_Elsewhere
+                    (state, bankEvent) = input;
+                    var balance = state.IsSome switch
+                    {
+                        true => state.ToList().Head().amount,
+                        _ => 0
+                    };
 
-                return bankEvent switch
+                    return bankEvent switch
+                    {
+                        MoneyWithdrawn m1 => Option<Account>.Some(new Account(balance - m1.Amount)),
+                        MoneyDeposited m3 => Option<Account>.Some(new Account(balance + m3.Amount)),
+                        _ => Option<Account>.None
+                    };
+                },
+                CommandHandler = input =>
                 {
-                    MoneyWithdrawn m1 => Option<Account>.Some(new Account(balance - m1.Amount)),
-                    MoneyDeposited m3 => Option<Account>.Some(new Account(balance + m3.Amount)),
-                    _ => Option<Account>.None
-                };
-            };
-            
-            
-            cqrsModel.CommandHandler = input =>
-            {
-                Option<Account> state;
-                BankCommand command;
-                (state, command) = input;
+                    Option<Account> state;
+                    BankCommand command;
+                    (state, command) = input;
                 
-                Lst<BankEvent> eventList;
-                // ReSharper disable once SuggestVarOrType_Elsewhere
-                Either<string, Lst<BankEvent>> result = Either<string, Lst<BankEvent>>.Bottom;
-                switch (state.IsNone)
-                {
-                    case true:
-                        switch (command)
-                        {
-                            case Deposit d:
-                                eventList = new Lst<BankEvent>
-                                {
-                                    new MoneyDeposited
-                                    {
-                                        Amount = d.Amount
-                                    }
-                                };
-                                result = Right<string, Lst<BankEvent>>(eventList);
-                                break;
-                            case Withdraw w:
-                                result = Left<string, Lst<BankEvent>>("You don't have an account so you can't withdraw money!");
-                                break;
-                        }
-                        break;
-                    case false:
-                        var actualState = state.ToList().Head();
-                        switch (command)
-                        {
-                            case Deposit d:
-                                eventList = new Lst<BankEvent>
-                                {
-                                    new MoneyDeposited
-                                    {
-                                        Amount = d.Amount
-                                    }
-                                };
-                                result = Right<string, Lst<BankEvent>>(eventList);
-                                break;
-                            case Withdraw w:
-                                if (w.Amount <= actualState.amount)
-                                {
+                    Lst<BankEvent> eventList;
+                    // ReSharper disable once SuggestVarOrType_Elsewhere
+                    Either<string, Lst<BankEvent>> result = Either<string, Lst<BankEvent>>.Bottom;
+                    switch (state.IsNone)
+                    {
+                        case true:
+                            switch (command)
+                            {
+                                case Deposit d:
                                     eventList = new Lst<BankEvent>
                                     {
-                                        new MoneyWithdrawn() 
+                                        new MoneyDeposited
                                         {
-                                            Amount = w.Amount
+                                            Amount = d.Amount
                                         }
                                     };
                                     result = Right<string, Lst<BankEvent>>(eventList);
-                                }
-                                else
-                                {
-                                    result = Left<string, Lst<BankEvent>>("You don't have enough money!");
-                                }
-                                break;
-                        }
-                        break;
+                                    break;
+                                case Withdraw w:
+                                    result = Left<string, Lst<BankEvent>>("You don't have an account so you can't withdraw money!");
+                                    break;
+                            }
+                            break;
+                        case false:
+                            var actualState = state.ToList().Head();
+                            switch (command)
+                            {
+                                case Deposit d:
+                                    eventList = new Lst<BankEvent>
+                                    {
+                                        new MoneyDeposited
+                                        {
+                                            Amount = d.Amount
+                                        }
+                                    };
+                                    result = Right<string, Lst<BankEvent>>(eventList);
+                                    break;
+                                case Withdraw w:
+                                    if (w.Amount <= actualState.amount)
+                                    {
+                                        eventList = new Lst<BankEvent>
+                                        {
+                                            new MoneyWithdrawn() 
+                                            {
+                                                Amount = w.Amount
+                                            }
+                                        };
+                                        result = Right<string, Lst<BankEvent>>(eventList);
+                                    }
+                                    else
+                                    {
+                                        result = Left<string, Lst<BankEvent>>("You don't have enough money!");
+                                    }
+                                    break;
+                            }
+                            break;
+                    }
+
+                    return result;
                 }
-
-                return result;
             };
-
         }
         
     }
