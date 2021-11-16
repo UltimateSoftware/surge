692561adaeca1d2c8e51d2e4ebdfe0a912f933a8
zachary-rote <zachary-rote@github.com>
2021-11-16 07:49:43 -0500

    experimental annotations

---
+++ modules/common/src/main/java/surge/annotations/Experimental.java
@@ -0,0 +1,15 @@
+package surge.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Experimental annotation used to denote that a particular class or method should be considered experimental to denote
+ * that it can change in backwards incompatible ways without a major version bump.
+ */
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Experimental {
+}
