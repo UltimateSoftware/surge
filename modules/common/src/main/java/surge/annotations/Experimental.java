4df669edbfe9170e1349bee14b92ff5efcc8aac1
zachary-rote <zachary-rote@github.com>
2021-11-16 10:27:46 -0500

    exclude specific annotations

--- modules/common/src/main/java/surge/annotations/Experimental.java
+++ modules/common/src/main/java/surge/annotations/Experimental.java
@@ -1,18 +1,15 @@
 package surge.annotations;
 
-import akka.annotation.ApiMayChange;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Experimental annotation used to indicate that a particular class or method should be considered experimental to denote
+ * Experimental annotation used to indicate that a particular class or method should be considered experimental denoting
  * that it can change in backwards incompatible ways without a major version bump.
  */
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@ApiMayChange
 public @interface Experimental {
 }
