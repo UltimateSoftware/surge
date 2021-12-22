ff07835580230adf7ab952e5e41bf7957cb44b81
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-22 18:43:27 +0100

    Update scalafmt-core to 3.0.8 (#172)
    * Update scalafmt-core to 3.0.8
    
    * Reformat with scalafmt 3.0.8

--- modules/command-engine/core/src/main/scala/surge/internal/persistence/PersistentActor.scala
+++ modules/command-engine/core/src/main/scala/surge/internal/persistence/PersistentActor.scala
@@ -310,7 +310,7 @@ class PersistentActor[S, M, E](
   private def handlePassivate(): Unit = {
     log.trace(s"PersistentActor for aggregate ${businessLogic.aggregateName} $aggregateId is passivating gracefully")
 
-    //FIXME: temporary fix to support switch between akka and existing shard allocation strategy
+    // FIXME: temporary fix to support switch between akka and existing shard allocation strategy
     if (isAkkaClusterEnabled) {
       context.parent ! Passivate(Stop)
     } else {
