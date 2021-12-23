d0da4523d0f7b727c683419c95f1282a6b2e17e4
Scala Steward <me@scala-steward.org>
2021-12-23 18:34:55 +0100

    Update logback-classic to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies extends AutoPlugin {
     val java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.1"
     val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka" % "2.8.1" % Test
     val junit = "junit" % "junit" % "4.13.2" % Test
-    val logback = "ch.qos.logback" % "logback-classic" % "1.2.9"
+    val logback = "ch.qos.logback" % "logback-classic" % "1.2.10"
     val json4s = "org.json4s" %% "json4s-native" % "4.0.3"
     val mockitoCore = "org.mockito" % "mockito-core" % "4.2.0"
     val scalatest = "org.scalatest" %% "scalatest" % "3.2.10" % Test
