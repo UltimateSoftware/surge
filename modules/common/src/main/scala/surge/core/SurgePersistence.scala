41914ef881982a53e12c795d2702049842e3c0c7
Sebastian Harko <sebastian.harko@protonmail.com>
2022-04-05 16:32:58 +0200

    change names

--- modules/common/src/main/scala/surge/core/SurgePersistence.scala
+++ modules/common/src/main/scala/surge/core/SurgePersistence.scala
@@ -4,9 +4,12 @@ import akka.Done
 
 import scala.concurrent.Future
 
-trait SurgePersistence[State, Event] {
+trait SurgeSnapshotting[State] {
   def persistState(state: Option[State]): Future[Done]
   def fetchState(id: String): Future[Option[State]]
+}
+
+trait SurgeEventPublishing[State, Event] extends SurgeSnapshotting[State] {
   def persistEvents(events: Seq[Event]): Future[Done]
-  def persist(events: Seq[Event], state: Option[State]): Future[Done]
+  def persistAll(events: Seq[Event], state: Option[State]): Future[Done]
 }
