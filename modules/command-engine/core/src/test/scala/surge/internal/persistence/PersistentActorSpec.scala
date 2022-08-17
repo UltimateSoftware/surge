db070579117ffaa51cf0f039c835a197745d7614
Jeff Boutotte <jeff.boutotte@ukg.com>
2022-08-17 12:47:03 -0400

    Filter events that shouldn't be persisted later in the flow (#284)

--- modules/command-engine/core/src/test/scala/surge/internal/persistence/PersistentActorSpec.scala
+++ modules/command-engine/core/src/test/scala/surge/internal/persistence/PersistentActorSpec.scala
@@ -144,7 +144,7 @@ class PersistentActorSpec
 
     probe.expectMsg(PersistentActor.ACKSuccess(expectedState))
     val expectedStateSerialized = businessLogic.serializeState(state.aggregateId, expectedState, mockProducer.assignedPartition).futureValue
-    val expectedEventSerialized = businessLogic.serializeEvents(Seq(expectedEvent -> businessLogic.kafka.eventsTopic)).futureValue
+    val expectedEventSerialized = businessLogic.serializeEvents(Seq(expectedEvent -> Some(businessLogic.kafka.eventsTopic))).futureValue
 
     val messagesCaptor: ArgumentCaptor[Seq[KafkaProducerActor.MessageToPublish]] = ArgumentCaptor.forClass(classOf[Seq[KafkaProducerActor.MessageToPublish]])
     verify(mockProducer).publish(any[UUID], argEquals(state.aggregateId), messagesCaptor.capture())
